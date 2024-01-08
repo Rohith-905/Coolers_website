40a7766ee576846041bd5fc13782ede897570204
Rohith <kagitapu.rohith905@gmail.com>
2024-01-09 00:02:14 +0530

    Report page

--- frontend/src/components/reportPage.js
+++ frontend/src/components/reportPage.js
@@ -1,15 +1,89 @@
-import React from "react";
 import AppBarPage from "./appBarPage";
 
-function ReportPage () {
 
-     
-    return(
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './reportPage.css';
+
+const ReportPage = () => {
+  const [data, setData] = useState({ vendorData: [], customerData: [] });
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const getTotalAmount = (data) => {
+    return data
+      .filter(row => row.name !== '' && row.amount !== '')
+      .reduce((acc, curr) => acc + parseInt(curr.amount), 0);
+  };
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/api/get_due_data');
+      setData(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  return (
+    <AppBarPage>
         <AppBarPage>
-            <div> in ReportPage</div>
-        </AppBarPage>
-        
-    );
-}
+      <div className="table-container">
+        <div>
+          <h2>Vendor Due</h2>
+          <p>Total Amount: {getTotalAmount(data.vendorData)}</p>
+          <table border="1">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Amount</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.vendorData
+                .filter(row => row.name !== '' && row.amount !== '')
+                .map((row, index) => (
+                  <tr key={index}>
+                    <td>{row.name}</td>
+                    <td>{row.amount}</td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
+
+        <div>
+          <h2>Customer Due</h2>
+          <p>Total Amount: {getTotalAmount(data.customerData)}</p>
+          <table border="1">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Amount</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.customerData
+                .filter(row => row.name !== '' && row.amount !== '')
+                .map((row, index) => (
+                  <tr key={index} style={{ color: row.amount > 0 ? 'red' : 'green' }}>
+                    <td>{row.name}</td>
+                    <td>{row.amount}</td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </AppBarPage>
+     
+
+   
+
+    </AppBarPage>
+  );
+};
 
 export default ReportPage;
